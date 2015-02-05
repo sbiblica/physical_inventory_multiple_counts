aa891e76e1bf9aa1f0338b069fce7a1d16f5bd9a
miguelchuga <miguelchuga@gmail.com>
2015-02-05 15:57:48 -0600

    subiendo el proyecto Miguel Chuga

---
+++ __openerp__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Physical Inventory With Multiple Counts',
+    'version': '1.0.8',
+    'author': 'acentoNET',
+    'summary': 'Inventory, Logistic, Storage',
+    'description' : """
+Physical Inventory with Multiple Counts
+=======================================
+    """,
+    'website': 'http://www.acentonet.com',
+    'category': 'Warehouse Management',
+    'depends': ["stock", "product"],
+    'init_xml' : [],
+    'demo_xml' : [],
+    'data': [
+        'stock_inventory_view_multicount.xml',
+        'physical_inventory_multicount_report.xml',
+        'views/report_inventory_counts_1_2.xml',
+        'views/report_inventory_count_3.xml',
+        'views/count_3_validation_template.xml',
+        'views/report_inventory_count_4.xml',
+        'report/count_3_differences_graph.xml',
+        'report/count_4_graph.xml',
+        'wizard/count_3_details_wizard.xml',
+        'wizard/count_3_validation_wizard.xml',
+    ],
+    'sequence': 150,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ wizard/count_3_details_wizard.py
@@ -0,0 +1,29 @@
+
+from openerp import tools
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class count_3_details_report(osv.osv_memory):
+
+    _name = "count_3.details.report"
+    _description = "Wizard that generates reports for physical inventory counts"
+    _columns = {
+        'inventory_id': fields.many2one('stock.inventory', 'Select inventory', ondelete='cascade', select=True),
+        'sector': fields.char('Select sector', size=100),
+    }
+
+    def open_table(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        data = self.read(cr, uid, ids, context=context)[0]
+        ctx = context.copy()
+        ctx['inventory_id'] = data['inventory_id']
+        ctx['sector'] = data['sector']
+        return {
+            'name': _('Inventory count 3 details'),
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'res_model': 'stock.inventory.count_3',
+            'type': 'ir.actions.act_window',
+            'context': ctx,
+        }
